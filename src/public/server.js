50ae6ace529fb12fb907b69f9496bd054bf1eca9
nandalalainfotech <nandalalainfotech@gmail.com>
2022-07-20 11:14:12 +0530

    changes are done

--- src/public/server.js
+++ src/public/server.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT ||  8081;
-app.listen(port, "0.0.0.0", function() {
+var PORT = process.env.PORT ||  8081;
+app.listen(PORT, "0.0.0.0", function() {
 // console.log("Listening on Port 3000");
 });
