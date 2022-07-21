e81ac2ebfb37fec1a2129f88ded5e0a1e1910c82
nandalalainfotech <nandalalainfotech@gmail.com>
2022-07-21 17:39:01 +0530

    correcting the changes for frontend (working)

--- src/index.js
+++ src/index.js
@@ -50,9 +50,9 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
 // // dotenv.config({ path: `${__dirname}/../env/.env.${process.env.NODE_ENV.trim()}` });
 
 
-// app.get('/', (req, res) => {
-//     res.sendFile('index.html', { root: `${__dirname}/public/dist/sirius` });
-// });
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: `${__dirname}/public/dist/sirius` });
+});
 
 // if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
 // // dotenv.config({ path: `${__dirname}/../env/.env.${process.env.NODE_ENV.trim()}` });
@@ -62,25 +62,26 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
 //     //   res.sendFile('index.html', { root: `${__dirname}/public/dist/omega` });
 //     res.sendFile(path.resolve(path.dirname('./src/public/dist/sirius')));
 // });
-app.use(express.static("./dist/sirius"));
-app.get("/*", function (req, res) {
-    res.sendFile("index.html", { root: "dist/sirius" });
-});
-// app.use(express.static(path.resolve(path.dirname('./src/public/dist/sirius'))));
-app.use(express.static("./public/dist/sirius"));
-
-app.get("/", function (req, res) {
-    res.sendFile("index.html", { root: "public/dist/sirius" });
-});
+app.use(express.static(path.resolve(path.dirname('./src/public/dist/sirius'))));
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(`${__dirname}/public/dist/omega`));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(path.dirname('./src/public/dist/sirius')));
+})
+
+// const express = require('express'); nor needed
 app.use(express.json());
 
 // not needed
-// app.get('/', (req, res) => {
-//     res.send('Backend welcomes you');
-// });
+app.get('/', (req, res) => {
+    res.send('Backend welcomes you');
+
+    // res.sendFile(path.join(__dirname, 'public/src/index.html'));
+});
 
 // needed
 
